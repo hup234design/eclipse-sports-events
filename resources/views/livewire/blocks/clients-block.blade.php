5b4ca170ef188aaad5f5596eb7a5ed7aeccd62c6
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-01-31 11:01:17 +0000

    clients styling

--- resources/views/livewire/blocks/clients-block.blade.php
+++ resources/views/livewire/blocks/clients-block.blade.php
@@ -1,10 +1,12 @@
 <div>
-    <div class="grid gap-12 lg:grid-cols-3">
+    <div class="md:columns-2 lg:columns-3 gap-8 space-y-8">
         @foreach($clients as $client)
-            <div class="prose max-w-none border border-gray-400 p-8">
-                <img src="{{ asset('storage/' . $client->logo) }}" class="w-full h-auto">
-                <h3>{{ $client->name }}</h3>
-                <p>{!! nl2br($client->description) !!}</p>
+            <div class="break-inside-avoid-column w-full p-8 border-2 border-gray-300">
+                <div class="prose max-w-none text-center">
+                    <img src="{{ asset('storage/' . $client->logo) }}" class="w-full h-auto">
+                    <h3>{{ $client->name }}</h3>
+                    <p>{!! nl2br($client->description) !!}</p>
+                </div>
             </div>
         @endforeach
     </div>
