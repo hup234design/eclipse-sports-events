d34164a40a2adb1b82ce665f5564554cf391e00d
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-02-09 22:02:12 +0000

    WIP

--- config/cms.php
+++ config/cms.php
@@ -4,6 +4,5 @@ return [
     'content_blocks' => [
         \App\Livewire\ClientsBlock::class,
         \App\Livewire\UpcomingEventsBlock::class,
-        \App\Livewire\LatestNewsBlock::class
     ],
 ];
